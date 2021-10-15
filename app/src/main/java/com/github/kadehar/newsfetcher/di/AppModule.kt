be775d2cb48d377d2055db7352e0c9848d665f42
Кузнецов Алексей Эдуардович <akuznetsov@mpl.lc>
2021-10-15 20:59:20 +0300

    add di and first fragment

---
+++ app/src/main/java/com/github/kadehar/newsfetcher/di/AppModule.kt
@@ -0,0 +1,22 @@
+package com.github.kadehar.newsfetcher.di
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+val appModule = module {
+    single<OkHttpClient> {
+        OkHttpClient.Builder()
+            .addInterceptor(HttpLoggingInterceptor())
+            .build()
+    }
+
+    single<Retrofit> {
+        Retrofit.Builder()
+            .client(get<OkHttpClient>())
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+}
